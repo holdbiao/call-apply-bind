cb213bb027bfffd025bb42febcb43eff786bc766
holdbiao <1007059411@qq.com>
2019-12-21 21:57:44 +0800

    增加兼容版bind

--- bind.js
+++ bind.js
@@ -19,3 +19,31 @@ Function.prototype.myBind = function (context = window) {
 
 test.bind(obj, 0)(1, 2) // this指向： obj (3) [0, 1, 2]
 test.myBind(obj, 0)(1, 2) // this指向： obj (3) [0, 1, 2]
+
+/**
+ * 1、已知 ES5 中 func.bind(context, 1, 2)(3, 4) 等价于 func.call(context, 1, 2, 3, 4)
+ * 请用 ES3 实现一个 bind 的 polyfill
+ */
+
+// 兼容版本
+Function.prototype.my_bind = function (context = window) {
+    // var args = Array.from(arguments) // 类数组转数组(es6) console.log(args instanceof Array)
+    var argumentsArr = Array.prototype.slice.call(arguments) // 类数组转数组
+    var args = argumentsArr.slice(1) // 后面的参数
+    var self = this // 调用的方法本身
+    return function () {
+        var newArgs = Array.prototype.slice.call(arguments) // 返回函数的arguments
+        self.apply(context, args.concat(newArgs)) // 合并两args
+    }
+}
+
+//  测试
+var name2 = 'window name'
+var obj2 = {
+    name2: 'obj name',
+}
+var fn = function () {
+    console.log(this.name2, [...arguments])
+}
+
+fn.my_bind(obj2)(2, 3)
